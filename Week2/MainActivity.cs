f88683cdf6a2adda161e87bd4f9dc644ca002988
Steves161 <27788517+Steves161@users.noreply.github.com>
2018-07-20 18:36:21 +1000

    Week 2 Creation
    The base of my assignment to be created from week 2 empty android project.

---
+++ Week2/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Week2
+{
+    [Activity(Label = "Assignment 1 Notepad App", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
